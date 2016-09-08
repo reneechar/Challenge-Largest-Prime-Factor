3ae76384e65765a0f0709344eaeb3b4240001df7
Renee <rchar20@gmail.com>
2016-09-08 10:46:25 -1000

    Changed outter while loop to a do loop so that the loop will run even when the number passed in is even.

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -3,12 +3,12 @@ exports.largestPrimeFactor = function(n){
   // do your work here
   var divsor = 2;
   
-  while (n%divsor !== 0) {
-  	divsor++;
+  do {
     while (n%divsor === 0 && divsor < (n/2)) {
   	  n /= divsor;
   	}
-  };
+  	divsor++;
+  } while (n%divsor !== 0);
 
   return n;
 };
