18dfdd0dd89a817bec786f69b0fbe33309d43a7c
Renee <rchar20@gmail.com>
2016-09-08 22:06:07 -1000

    added removeEven Factors function and divideNew function to account for even numbers as parameters

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,14 +1,34 @@
 exports.largestPrimeFactor = function(n){
-
-  // do your work here
-  var divsor = 2;
   
-  do {
-    while (n%divsor === 0 && divsor < (n/2)) {
-  	  n /= divsor;
-  	}
-  	divsor++;
-  } while (n%divsor !== 0);
+  /*divides the parameter n by 2 until n is no longer divisible by 2.
+  *thus removing all even factors in n.
+  */
+  function removeEvenFactors() {
+    while(n%2 === 0) {
+      n /= 2;
+    };
+  }
+  removeEvenFactors();
 
+  //divisor starts at 3 since we have removed all even factors
+  var divisor = 3;
+
+  /*divideNew will divide n by the divisor if the divisor is a multiple and in range.
+  *if n is not divisible by the divisor, it will increment the divisor to the next odd number above.
+  */
+  function divideNew() {
+    if (n % divisor === 0 && divisor < (n/2)) {
+      n /= divisor;
+    } else {
+      if (n % divisor !== 0) {
+        divisor +=2;
+      }
+    }
+  }
+
+  //This while loop is to run divideNew until n is no longer divisible by any possible divisors.
+  while(divisor<(n/2)){
+    divideNew();
+  }
   return n;
 };
